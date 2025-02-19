bf3c12d5977728b6090b3a5eb70460b58b9e5b77
Dharshinisri G <dharshinisrigajendran@gmail.com>
2025-02-19 20:22:31 +0530

    Added SumOfNums

---
+++ src/Programs/SumOfNums.java
@@ -0,0 +1,10 @@
+package Programs;
+import java.util.*;
+public class SumOfNums{
+    public static void main(String args[]){
+        Scanner x=new Scanner(System.in);
+        int a=x.nextInt();
+        int b=x.nextInt();
+        System.out.println(a+b);
+    }
+}
